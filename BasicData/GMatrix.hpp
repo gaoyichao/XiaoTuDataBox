0a675114c8ca1492c57a6cd97c980f9f9df09653
GAO Yichao <gaoyichao2@gmail.com>
2019-09-04 18:42:02 +0800

    GMatrix Functor

--- BasicData/GMatrix.hpp
+++ BasicData/GMatrix.hpp
@@ -5,28 +5,35 @@
 #include <Array2D.hpp>
 #include <DataArray.hpp>
 
-template <class NodeType, class EdgeType>
+template <class NodeType, class EdgeType, class FillZeroEdgeFunc, class IsZeroEdgeFunc>
 class GMatrix
 {
     public:
-        GMatrix() {};
-
-        GMatrix(int no) : nodes(no, 0), m(no, no, NULL) {
-
+        GMatrix() {}
+        GMatrix(int n) : nodes(n), matrix(n, n)
+        {
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    FillZeroEdge(matrix(i, j));
         }
-
 
         friend std::ostream & operator << (std::ostream & stream, GMatrix const & gm)
         {
-            stream << "nodes:" << std::hex << gm.nodes << std::dec << std::endl;;
-            stream << std::hex << gm.m << std::dec;
+            stream << "nodes:" << std::hex << gm.nodes << std::dec << std::endl;
+            stream << std::hex << gm.matrix << std::dec;
             return stream;
         }
 
+        EdgeType & operator () (int i, int j) { return matrix(i, j); }
+
+        bool IsConnected(int i, int j) { return !IsZeroEdge(matrix(i, j)); }
 
     protected:
-        Array2D<EdgeType *> m;
+        Array2D<EdgeType> matrix;
         DataArray<NodeType> nodes;
+
+        FillZeroEdgeFunc FillZeroEdge;
+        IsZeroEdgeFunc IsZeroEdge;
 };
 
 
--- Tester/t_GMatrix.cpp
+++ Tester/t_GMatrix.cpp
@@ -1,14 +1,47 @@
 #include <gtest/gtest.h>
 
+#include <locale>
+#include <cmath>
 #include <GMatrix.hpp>
+
+class DoubleIsZeroEdge {
+    public:
+        bool operator() (double const & v)
+        {
+            return std::isnan(v);
+        }
+};
+
+class DoubleFillZeroEdge {
+    public:
+        void operator() (double & v)
+        {
+            v = std::nan("");
+        }
+};
+
+
+
 
 TEST(GMatrix, init)
 {
     int no = 10;
 
-    GMatrix<double, double> *gmatrix = new GMatrix<double, double>(no);
+    GMatrix<double, double, DoubleFillZeroEdge, DoubleIsZeroEdge> *gmatrix = new GMatrix<double, double, DoubleFillZeroEdge, DoubleIsZeroEdge>(no);
 
     std::cout << *gmatrix << std::endl;
+
+    std::cout << std::numeric_limits<double>::min() << std::endl;
+    double na = std::nan("");
+    bool isna = (na == std::nan(""));
+    unsigned long long *pna = (unsigned long long*)&na;
+    std::cout << na << std::endl;
+    std::cout << std::hex << *pna << std::dec << std::endl;
+    std::cout << (isna ? "yes" : "no") << std::endl;
+
+    DoubleIsZeroEdge dze;
+    isna = dze(na);
+    std::cout << (isna ? "yes" : "no") << std::endl;
 
     delete gmatrix;
 }
